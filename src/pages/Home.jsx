309136bd86dd2b5852a74c953acb6bf6d1283c96
Saw Daniel <sawdaniel@Passo.local>
2024-09-01 09:48:08 +0700

    forth 5649

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostItem from "../components/PostItem";
 
 const Home = () => {
-    const [ posts, setPosts] = useState(
-        [
-            {
-                title: "Game and books",
-                body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptate. Deserunt maiores nulla ipsum vero aliquid, nesciunt quaerat placeat vitae excepturi, ex autem nemo numquam sapiente ea aspernatur facilis eveniet.",
-                created_at: "10/25/2023",
-                id: 1,
-            },
-            {
-                title: "Best JS framework",
-                body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum officia id atque hic fugiat nam temporibus veritatis quis incidunt. Ea nemo labore tempore consectetur libero amet officiis quae ipsum ullam, fuga voluptatem. Quidem voluptatibus laudantium at expedita nisi laboriosam?",
-                created_at: "11/25/2023",
-                id: 2,
-            }
-        ]
-    )
+    const [ posts, setPosts] = useState([])
+    useEffect (() => {
+        fetch("http://localhost:3000/posts")
+            .then(res=>res.json())
+            .then(data=>setPosts(data))
+    }, [])
     const handleDelete = (id) => {
         setPosts(posts.filter((posts) => posts.id !== id))
+        fetch("http://localhost:3000/posts/" + id , {
+            method: 'DELETE'
+        })
     }
     return (
         <>
